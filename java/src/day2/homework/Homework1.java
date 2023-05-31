dc029737b9120ebba1ec314fa17071b62ae6281c
kjy0411 <134986647+kjy0411@users.noreply.github.com>
2023-05-31 15:13:27 +0900

    자바 : 2일차 숙제

---
+++ java/src/day2/homework/Homework1.java
@@ -0,0 +1,17 @@
+package day2.homework;
+
+import java.util.Scanner;
+
+public class Homework1 {
+
+	public static void main(String[] args) {
+		/*콘솔에서 정수 2개와 문자(산술연산자)를 입력받아 출력하는 코드를 작성하세요
+		*ex) 두 정수와 산술연산자를 입력하세요 (예 : 1 + 2) :
+		*[입력] 1 + 2 [엔터]
+		*[출력] 1 + 2
+		*/
+		Scanner sc = new Scanner(System.in);
+		
+	}
+
+}
---
+++ java/src/day2/homework/Homework2.java
@@ -0,0 +1,20 @@
+package day2.homework;
+
+public class Homework2 {
+
+	public static void main(String[] args) {
+		/*두 정수를 입력받아 두 정수의 산술 연산 결과를 출력하는 코드를 작성하세요.
+		 *단, 2번째 숫자는 0이 아님.
+		 *ex) 두 정수를 입력하세요.
+		 *[입력] 1 2 [엔터]
+		 *[출력]
+		 * 1 + 2 = 3
+		 * 1 - 2 = -1
+		 * 1 * 2 = 2
+		 * 1 / 2 = 0.5
+		 * 1 % 2 = 1  
+		 */
+
+	}
+
+}
