f4e767e1d58ac875f45c439757ca804b7bde516f
kjy0411 <134986647+kjy0411@users.noreply.github.com>
2023-06-01 12:48:08 +0900

    숙제&풀이

--- java/src/day2/homework/Ex1.java
+++ java/src/day2/homework/Ex1.java
@@ -2,7 +2,7 @@ package day2.homework;
 
 import java.util.Scanner;
 
-public class Homework1 {
+public class Ex1 {
 
 	public static void main(String[] args) {
 		/*콘솔에서 정수 2개와 문자(산술연산자)를 입력받아 출력하는 코드를 작성하세요
@@ -13,12 +13,15 @@ public class Homework1 {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("두 정수와 산술연산자를 입력하세요 (예 : 1 + 2)");
+		
 		int num1 = sc.nextInt();
 		char ch = sc.next().charAt(0);
 		int num2 = sc.nextInt();
+		
 		System.out.println("" + num1 + " " + ch + " " + num2);
 		
 		sc.close();
+		
 	}
 
 }
--- java/src/day2/homework/Ex2.java
+++ java/src/day2/homework/Ex2.java
@@ -2,7 +2,7 @@ package day2.homework;
 
 import java.util.Scanner;
 
-public class Homework2 {
+public class Ex2 {
 
 	public static void main(String[] args) {
 		/*두 정수를 입력받아 두 정수의 산술 연산 결과를 출력하는 코드를 작성하세요.
@@ -22,11 +22,17 @@ public class Homework2 {
 		int num1 = sc.nextInt();
 		int num2 = sc.nextInt();
 		
-		System.out.println(num1 + " + " + num2 + " = " + (num1+num2));
-		System.out.println(num1 + " - " + num2 + " = " + (num1-num2));
-		System.out.println(num1 + " * " + num2 + " = " + (num1*num2));
-		System.out.println(num1 + " / " + num2 + " = " + ((double)num1/num2));
-		System.out.println(num1 + " % " + num2 + " = " + (num1%num2));
+		int sum = num1 + num2;
+		int sub = num1 - num2;
+		int mul = num1 * num2;
+		double div = (double)num1 / num2;	// 1/2의 결과가 0이어서, 1.0/2로 바꾸어 0.5가 나오게 함
+		int mod = num1 % num2;
+		
+		System.out.println(num1 + " + " + num2 + " = " + sum);
+		System.out.println(num1 + " - " + num2 + " = " + sub);
+		System.out.println(num1 + " * " + num2 + " = " + mul);
+		System.out.println(num1 + " / " + num2 + " = " + div);
+		System.out.println(num1 + " % " + num2 + " = " + mod);
 		
 		sc.close();
 
