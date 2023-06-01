73e7efbe1e624c07247143567a904bb9c3a60369
kjy0411 <134986647+kjy0411@users.noreply.github.com>
2023-06-01 15:02:01 +0900

    day3 숙제

---
+++ java/src/day3/homework/Ex1_conditional.java
@@ -0,0 +1,19 @@
+package day3.homework;
+
+public class Ex1_conditional {
+
+	public static void main(String[] args) {
+		/*성별(M:남성, W:여성)를 입력받아 여성인지 확인하는 코드를 작성하세요.
+		 * 조건 연산자 이용
+		 * ex)
+		 * input gender :
+		 * M
+		 * Are you a women? man
+		 * input gender :
+		 * W
+		 * Are you a women? woman
+		 */
+
+	}
+
+}
