4108e7c41d9b76ed7a45a59dca560dcdd1980dbc
kjy0411 <134986647+kjy0411@users.noreply.github.com>
2023-06-01 14:57:35 +0900

    자바 : 홀, 짝 판별 예제

---
+++ java/src/day3/pratice/Ex4_even.java
@@ -0,0 +1,24 @@
+package day3.pratice;
+
+import java.util.Scanner;
+
+public class Ex4_even {
+
+	public static void main(String[] args) {
+		//정수 num를 입력받아 num가 홀수인지 짝수인지 판별하는 코드를 작성하세요.
+		//num을 2로 [나누었을 때 나머지가] 0이 아니면 홀수
+		//num을 2로 [나누었을 때 나머지가] 0이면 짝수
+		Scanner sc = new Scanner(System.in);
+		System.out.println("정수를 입력하세요 : ");
+		int num = sc.nextInt();
+		if(num % 2 == 0 ) {
+			System.out.println("정수 " + num + " 은 짝수입니다.");
+		}
+		if(num % 2 == 1 ) {
+			System.out.println("정수 " + num + " 은 홀수입니다.");
+		}
+		
+		sc.close();
+	}
+
+}
