a36aaa789896bc3d89fce96beee9e7bc01fc259e
kjy0411 <134986647+kjy0411@users.noreply.github.com>
2023-06-01 14:56:53 +0900

    자바 : if문 예제 (0판별)

---
+++ java/src/day3/ifex/IfEx1.java
@@ -0,0 +1,20 @@
+package day3.ifex;
+
+public class IfEx1 {
+
+	public static void main(String[] args) {
+		//정수 num가 0이면 0입니다라고 풀력하는 코드
+		int num = 10;
+		
+		if(num == 0) {
+			//0입니다 라고 출력
+			System.out.println("정수 " + num + " 은 0입니다.");
+		}
+		//정수 num가 0이 아니면 0이 아니라고 출력하는 코드
+		if(num != 0) {
+			System.out.println("정수 " + num + " 은 0이 아닙니다.");
+		}		
+			
+	}
+
+}
