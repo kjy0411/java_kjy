ae674006aae9e5cbe6ea4a3944dc560b4bff1e5f
kjy0411 <134986647+kjy0411@users.noreply.github.com>
2023-06-02 13:32:59 +0900

    기타

--- java/src/day3/practice/Ex2.java
+++ java/src/day3/practice/Ex2.java
@@ -1,25 +1,25 @@
-package day3.pratice;
+package day3.practice;
 
 import java.util.Scanner;
 
-public class Ex1 {
+public class Ex2 {
 
 	public static void main(String[] args) {
 		/* 성적을 입력받아 입력받은 성적이 60점 이상인지 확인하는 코드를 작성하세요
 		 * 예:
 		 * input score :
 		 * 65
-		 * Do you pass 65 points? true
+		 * Do you pass 65 points? pass
 		 * 
 		 * input score :
 		 * 45
-		 * Do you pass 45 points? false
+		 * Do you pass 45 points? fail
 		 */
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("input score : ");
 		int score = sc.nextInt();
-		boolean result = score >= 60;
+		String result = score >= 60 ? "pass" : "fail";
 		
 		System.out.println("Do you pass " + score + " points? : " + result);
 		
--- java/src/day3/practice/Ex3.java
+++ java/src/day3/practice/Ex3.java
@@ -1,4 +1,4 @@
-package day3.pratice;
+package day3.practice;
 
 import java.util.Scanner;
 
