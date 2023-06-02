3fa3f940ce6bdd4859b4e433cf198f362dabbe46
kjy0411 <134986647+kjy0411@users.noreply.github.com>
2023-06-02 13:46:37 +0900

    if문 예제 : 홀짝판별

---
+++ java/src/day4/ifex/EvenNumberEx.java
@@ -0,0 +1,18 @@
+package day4.ifex;
+
+public class EvenNumberEx {
+
+	public static void main(String[] args) {
+		//if else문을 이용한 홀짝 판별 예제
+		int num = 4;
+		if(num % 2 == 0) {
+			System.out.println(num + " is a even number");
+		}
+		else {
+			System.out.println(num + " is a odd number");
+		}
+		
+
+	}
+
+}
