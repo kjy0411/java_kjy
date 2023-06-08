b718ed100f002965092eac583ee4755eab7161de
kjy0411 <134986647+kjy0411@users.noreply.github.com>
2023-06-08 09:14:57 +0900

    과제 풀이 - 서로소 판별(while문/for문)

---
+++ java/src/day6/homework/DisjointAwnser.java
@@ -0,0 +1,50 @@
+package day6.homework;
+
+import java.util.Scanner;
+
+public class DisjointAwnser {
+
+	public static void main(String[] args) {
+		/* 두 정수를 입력받아 두 정수가 서로소 관계인지 판별하는 코드를 작성하세요.
+		 * 서로소 : 최대 공약수가 1인 두 수의 관계
+		 * input 2 numbers : 3 4
+		 * Disfiont!(서로소)
+		 * input 2 numbers : 8 4
+		 * Not disfoint!(서로소 아님)
+		 */
+		
+		/* 반복횟수 : i는 1부터 num1까지 1씩 증가
+		 * 규칙성 : i가 num1의 약수이고 i가 num2의 약수이면 i를 gcd에 저장
+		 * 		num1을 i로 나누었을 때 나머지가 0과 같고
+		 * 		num2를 i로 나누었을 때 나머지가 0과 같다면
+		 * 		i를 gcd 에 저장
+		 * 반복문 종료 후 : gcd가 1이면 Disjoint를 출력하고,
+		 * 				아니면 Not disjoint를 출력
+		 */
+		
+		int num1, num2, gcd = 1, i = 1;
+		Scanner sc = new Scanner(System.in);
+		//두 정수를 입력
+		System.out.println("input 2 number : ");
+		num1 = sc.nextInt();
+		num2 = sc.nextInt();
+		
+		while(i <= num1) {
+			//num1을 i로 나누었을 때 나머지가 0과 같고
+			//num2를 i로 나누었을 때 나머지가 0과 같다면
+			if(num1 % i == 0 && num2 % i == 0) {
+				//gcd에 i를 저장
+				gcd = i;
+			}
+			i++;
+		}
+		//gcd가 1이면 Disjoint를 출력하고, 아니면 Not disjoint를 출력
+		if(gcd == 1) {
+			System.out.println("Disjoint!");
+		}else {
+			System.out.println("Not jisjoint!");
+		}
+		sc.close();
+	}
+
+}
