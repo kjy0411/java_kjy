46f567d7a90c1d4d9126a351d48e7eb10bc81658
kjy0411 <134986647+kjy0411@users.noreply.github.com>
2023-06-14 12:46:03 +0900

    수정

--- java/src/day5/practice/RandomEx.java
+++ java/src/day5/practice/RandomEx.java
@@ -9,7 +9,7 @@ public class RandomEx {
 		 * 0 * (max - min + 1) <= r * (max - min + 1) < 1 * (max - min + 1) 
 		 * 0 <= r * (max - min + 1) < max - min + 1
 		 * 0 + min <= r * (max - min + 1) + min  < max - min + 1 + min 
-		 * min <= r * (max - min + 1 + min) < max + 1
+		 * min <= r * (max - min + 1) + min < max + 1
 		 */
 		int min = 1, max = 100;
 		int random = (int)(Math.random() * (max - min + 1) + min);
