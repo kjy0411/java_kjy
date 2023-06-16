a8a19f9bda6884c210d222ff9f875f6aa517232f
kjy0411 <134986647+kjy0411@users.noreply.github.com>
2023-06-16 10:37:05 +0900

    day12 과제 완료

--- java/src/day12/homework/LottoEx.java
+++ java/src/day12/homework/LottoEx.java
@@ -17,22 +17,72 @@ public class LottoEx {
 		 * number : [1, 3, 40, 23, 24, 45]
 		 * bonus : 2
 		 * user : 1 2 3 4 5 6
-		 * losing ticker!]
+		 * losing ticker!
 		 * user : 1 3 40 23 24 2
 		 * 2!
 		 */
 		Scanner sc = new Scanner(System.in);
-		int com[] = new int[7];
-		int user[] = new int[6];
+		
+		int com[] = new int[7];		//로또 번호
+		int user[] = new int[6];	//사용자가 입력한 번호
 		//컴퓨터 번호 생성
 		Array.createRandonArray(1, 45, com);
+		//사용자 번호 입력
+		System.out.println("input (1~45)numbers : ");
+		for(int i = 0; i < user.length;) {
+			int num = sc.nextInt();
+			//1~45 범위 밖의 숫자를 입력하면 경고
+			if(num > 45 || num < 1) {
+				System.out.println("input (1~45)numbers : ");
+				continue;
+			}
+			//이미 입력한 숫자를 다시 입력하면 경고
+			if(Array.contains(user, num, i)) {
+				System.out.println("don't input same number!");
+				continue;
+			}else {
+				user[i] = num;
+				i++;
+			}
+		}
+		System.out.print("Lotto : ");
 		Array.printArray(com);
-		
-		//반복문
-			//사용자 번호 입력
-			
-			
-		
+		System.out.print("User : ");
+		Array.printArray(user);
+
+		int count = 0;			//당첨번호 개수
+		boolean bonus = false;	//보너스번호
+		//당첨번호 개수 확인
+		for(int i = 0; i < user.length; i++ ) {
+			if(Array.contains(user, com[i], user.length)){
+				count++;
+			}
+		}
+		//보너스번호 확인
+		if(Array.contains(user, com[6], user.length)) {
+			bonus = true;
+		}
+		//당첨번호 개수와 보너스 유무에 따른 결과 출력
+		System.out.print("result : ");
+		switch(count) {
+		case 6 :
+			System.out.println(1 + "!");
+			break;
+		case 5 :
+			System.out.println(bonus ? 2 + "!":3 + "!");
+			break;
+		case 4 : 
+			System.out.println(4 + "!");
+
+			break;
+		case 3 :
+			System.out.println(5 + "!");
+
+			break;
+		default : 
+			System.out.println("losing ticker!");
+		}
+
 		
 		sc.close();
 	}
