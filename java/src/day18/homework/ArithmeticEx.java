ac1d15337cd0a92d3ebb4469ab6094f27b858603
kjy0411 <134986647+kjy0411@users.noreply.github.com>
2023-06-23 14:57:47 +0900

    day18.과제 - 인터페이스

---
+++ java/src/day18/homework/ArithmeticEx.java
@@ -0,0 +1,30 @@
+package day18.homework;
+
+public class ArithmeticEx {
+
+	public static void main(String[] args) {
+		//산술연산을 하는 프록램을 작성하세요.
+		//인터페이스 이용
+		/* public interface Program{
+		 * 		interface run();
+		 * }
+		 * 1. Input Integer
+		 * 2. Input Operator
+		 * 3. Exit
+		 * 
+		 * menu : 1
+		 * 
+		 * input : 1 2
+		 * 
+		 * 1. Input Integer
+		 * 2. Input Operator
+		 * 3. Exit
+		 * 
+		 * menu : 2
+		 * 
+		 * input : +
+		 */
+
+	}
+
+}
