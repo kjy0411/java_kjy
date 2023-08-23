888045f94560c5a6d943303630d5cac93b46b593
kjy0411 <134986647+kjy0411@users.noreply.github.com>
2023-08-23 17:39:19 +0900

    servlet - 회원 가입/탈퇴 구현

---
+++ servlet/project/src/main/java/kr/kh/app/controller/Home.java
@@ -0,0 +1,43 @@
+package kr.kh.app.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import kr.kh.app.service.MemberService;
+import kr.kh.app.service.MemberServiceImp;
+
+public class Home extends HttpServlet {
+
+	public Home() {
+        super();
+    }
+
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init 실행");
+	}
+
+	public void destroy() {
+		System.out.println("distroy 실행");
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//서버에서 화면에게 데이터를 전송
+		request.setAttribute("mgs", "Hello");
+		//미리 만들어 놓은 home.jsp와 연결
+		request.getRequestDispatcher("WEB-INF/views/home.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// place your code here
+		//화면에서 보낸 id를 가져옴
+		String id = request.getParameter("id");
+		System.out.println(id);
+		doGet(request, response);
+	}
+
+}
